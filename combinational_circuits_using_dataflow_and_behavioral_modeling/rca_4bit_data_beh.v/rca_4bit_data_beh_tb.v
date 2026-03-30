0ce838d99f095e8489d0919f24c9884c720d0891
mohammedirfanvlsi <mohammedirfan0719@gmail.com>
2026-03-30 07:10:08 +0000

    rca and full subtractor and bcd has completed

---
+++ combinational_circuits_using_dataflow_and_behavioral_modeling/rca_4bit_data_beh.v/rca_4bit_data_beh_tb.v
@@ -0,0 +1,28 @@
+module rca_4bit_data_beh_tb;
+
+reg [3:0]a,b;
+reg cin;
+wire [3:0]sum_da;
+wire carry_da;
+wire[3:0]sum_be;
+wire carry_be;
+
+rca_4bit_top dut(.a(a),.b(b),.cin(cin),.sum_da(sum_da),.carry_da(carry_da),.sum_be(sum_be),.carry_be(carry_be));
+
+integer i;
+initial begin
+
+	$dumpfile("rca_4bit_data_beh.vcd");
+	$dumpvars(0,rca_4bit_data_beh_tb);
+
+	$monitor("time=%0t | a=%b,b=%b,cin=%b | sum_da=%b,carry_da=%b,sum_be=%b,carry_be=%b",$time,a,b,cin,sum_da,carry_da,sum_be,carry_be);
+
+for(i=0;i<16;i=i+1)
+begin
+	{a,b,cin}=i;#10;
+
+end
+$finish;
+end
+endmodule
+
